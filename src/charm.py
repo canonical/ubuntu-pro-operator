1d74b4ee3c1f1dba9f09c2339bf1365295f17d0b
Colin Misare <colin.misare@canonical.com>
2021-02-01 02:36:32 -0700

    changes based on feedback

--- src/charm.py
+++ src/charm.py
@@ -3,12 +3,13 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import hashlib
 import json
 import logging
-import shlex
 import subprocess
 
 from ops.charm import CharmBase
+from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus
 
@@ -16,33 +17,66 @@ from ops.model import ActiveStatus, BlockedStatus
 logger = logging.getLogger(__name__)
 
 
+# TODO - this output has fewer details than ubuntu-advantage status --all
+def parse_status():
+    """Determine the enabled set of ubuntu-advantage services"""
+    output = subprocess.check_output(["ubuntu-advantage", "status", "--format", "json"])
+    if isinstance(output, bytes):
+        output = output.decode("utf-8")
+    return json.loads(output)
+
+
 class UbuntuAdvantageCharm(CharmBase):
+    """Charm to handle ubuntu-advantage installation and configuration"""
+    _state = StoredState()
+
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.install, self.install)
+        self._state.set_default(ppa=None, hashed_token=None)
         self.framework.observe(self.on.config_changed, self.config_changed)
 
-    def install(self, event):
-        logger.info('Beginning install')
-        subprocess.check_call(['add-apt-repository', '--yes', 'ppa:ua-client/stable'])
-        subprocess.check_call(['apt', 'update'])
-        subprocess.check_call(['apt', 'install', '--yes', '--quiet', 'ubuntu-advantage-tools'])
-        self.unit.status = ActiveStatus('installed')
-        logger.info('Finished install')
-
     def config_changed(self, event):
-        logger.info('Beginning config_changed')
-        token = self.config.get('ubuntu-advantage-token')
-        if not token:
-            self.unit.status = BlockedStatus('No token configured')
+        """Install and configure ubuntu-advantage tools and attachment"""
+        logger.info("Beginning config_changed")
+        ppa = self.config.get("ppa")
+        if not ppa:
+            self.unit.status = BlockedStatus("No ppa configured")
             return
-        output = subprocess.check_output(['ubuntu-advantage', 'status', '--format', 'json'])
-        status = json.loads(output)
-        if not status['attached']:
-            subprocess.check_call(['ubuntu-advantage', 'attach', shlex.quote(token)])
-        self.unit.status = ActiveStatus('attached')
-        logger.info('Finished config_changed')
+        old_ppa = self._state.ppa
+        if old_ppa != ppa:
+            logger.info("Configuring ppa: %s", ppa)
+            subprocess.check_call(["add-apt-repository", "--yes", ppa])
+            subprocess.check_call(["apt", "update"])
+            logger.info("Installing ubuntu-advantage-tools")
+            subprocess.check_call(["apt", "install", "--yes", "--quiet", "ubuntu-advantage-tools"])
+            self._state.ppa = ppa
+            logger.info("Installed ubuntu-advantage-tools")
+        token = self.config.get("token")
+        if not token:
+            self.unit.status = BlockedStatus("No token configured")
+            return
+        hashed_token = hashlib.sha256(token.encode("utf-8")).hexdigest()
+        old_hashed_token = self._state.hashed_token
+        status = parse_status()
+        if status["attached"] and old_hashed_token != hashed_token:
+            logger.info("Detaching ubuntu-advantage subscription in preparation for reattachment")
+            subprocess.check_call(["ubuntu-advantage", "detach", "--assume-yes"])
+            status = parse_status()
+        if not status["attached"]:
+            return_code = subprocess.call(["ubuntu-advantage", "attach", token])
+            if return_code != 0:
+                self.unit.status = BlockedStatus("Error attaching, possibly an invalid token?")
+                return
+            self._state.hashed_token = hashed_token
+            status = parse_status()
+        services = []
+        for service in status["services"]:
+            if service["status"] == "enabled":
+                services.append(service["name"])
+        message = "attached (" + ",".join(services) + ")"
+        self.unit.status = ActiveStatus(message)
+        logger.info("Finished config_changed")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: nocover
     main(UbuntuAdvantageCharm)
--- tests/test_charm.py
+++ tests/test_charm.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 from subprocess import CalledProcessError
 from unittest import TestCase
 from unittest.mock import patch, call
@@ -16,48 +17,195 @@ class TestCharm(TestCase):
         self.harness = Harness(UbuntuAdvantageCharm)
         self.addCleanup(self.harness.cleanup)
 
-    @patch('subprocess.check_call')
-    def test_install(self, _check_call):
+    @patch("subprocess.check_call")
+    def test_config_changed_no_ppa(self, _check_call):
         self.harness.begin()
-        self.harness.charm.on.install.emit()
+        self.harness.update_config({"ppa": ""})
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+        self.assertEqual(self.harness.model.unit.status.message, "No ppa configured")
+
+    @patch("subprocess.check_call")
+    def test_config_changed_no_token(self, _check_call):
+        self.harness.begin()
+        self.harness.update_config({"ppa": "ua-client/stable"})
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+        self.assertEqual(self.harness.model.unit.status.message, "No token configured")
+
+    @patch("subprocess.call")
+    @patch("subprocess.check_output")
+    @patch("subprocess.check_call")
+    def test_config_changed_fresh_install(self, _check_call, _check_output, _call):
+        success = {
+            "attached": True,
+            "services": [
+                {
+                    "name": "esm-apps",
+                    "status": "enabled"
+                },
+                {
+                    "name": "esm-infra",
+                    "status": "enabled"
+                },
+                {
+                    "name": "livepatch",
+                    "status": "enabled"
+                }
+            ]
+        }
+        _check_output.side_effect = [
+            '{"attached":false}',
+            json.dumps(success)
+        ]
+        _call.return_value = 0
+        self.harness.begin()
+        self.harness.update_config({"token": "test-token"})
         self.assertEqual(_check_call.call_count, 3)
         _check_call.assert_has_calls([
-            call(['add-apt-repository', '--yes', 'ppa:ua-client/stable']),
-            call(['apt', 'update']),
-            call(['apt', 'install', '--yes', '--quiet', 'ubuntu-advantage-tools'])
+            call(["add-apt-repository", "--yes", "ppa:ua-client/stable"]),
+            call(["apt", "update"]),
+            call(["apt", "install", "--yes", "--quiet", "ubuntu-advantage-tools"])
         ])
+        self.assertEqual(_call.call_count, 1)
+        _call.assert_has_calls([
+            call(["ubuntu-advantage", "attach", "test-token"])
+        ])
+        self.assertEqual(self.harness.charm._state.ppa, "ppa:ua-client/stable")
+        self.assertEqual(self.harness.charm._state.hashed_token,
+                         "4c5dc9b7708905f77f5e5d16316b5dfb425e68cb326dcd55a860e90a7707031e")
         self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
+        self.assertEqual(self.harness.model.unit.status.message,
+                         "attached (esm-apps,esm-infra,livepatch)")
 
-    @patch('subprocess.check_call')
-    def test_install_fails(self, _check_call):
-        _check_call.side_effect = CalledProcessError('apt failure', 'add-apt-repository')
+    @patch("subprocess.check_call")
+    def test_config_changed_apt_failure(self, _check_call):
+        _check_call.side_effect = CalledProcessError("apt failure", "add-apt-repository")
         self.harness.begin()
         with self.assertRaises(CalledProcessError):
-            self.harness.charm.on.install.emit()
+            self.harness.update_config({"token": "test-token"})
         self.assertIsInstance(self.harness.model.unit.status, MaintenanceStatus)
 
-    def test_config_changed_no_token(self):
+    @patch("subprocess.check_call")
+    def test_config_changed_unmodified_ppa(self, _check_call):
         self.harness.begin()
-        self.harness.update_config({"ubuntu-advantage-token": ""})
-        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        self.harness.update_config({"ppa": "ppa:ua-client/stable"})
+        self.assertEqual(_check_call.call_count, 0)
+        self.assertEqual(self.harness.charm._state.ppa, "ppa:ua-client/stable")
 
-    @patch('subprocess.check_output')
-    def test_config_changed_already_attached(self, _check_output):
-        _check_output.return_value = '{"attached":true}'
+    @patch("subprocess.check_call")
+    def test_config_changed_modified_ppa(self, _check_call):
         self.harness.begin()
-        self.harness.update_config({'ubuntu-advantage-token': 'test-token'})
-        self.assertEqual(_check_output.call_count, 1)
-        _check_output.assert_called_with(['ubuntu-advantage', 'status', '--format', 'json'])
-        self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        self.harness.update_config({"ppa": "ppa:different-client/unstable"})
+        self.assertEqual(_check_call.call_count, 3)
+        _check_call.assert_has_calls([
+            call(["add-apt-repository", "--yes", "ppa:different-client/unstable"]),
+            call(["apt", "update"]),
+            call(["apt", "install", "--yes", "--quiet", "ubuntu-advantage-tools"])
+        ])
+        self.assertEqual(self.harness.charm._state.ppa, "ppa:different-client/unstable")
 
-    @patch('subprocess.check_call')
-    @patch('subprocess.check_output')
-    def test_config_changed_attach(self, _check_output, _check_call):
-        _check_output.return_value = '{"attached":false}'
+    @patch("subprocess.check_call")
+    def test_config_changed_modified_ppa_apt_failure(self, _check_call):
+        _check_call.side_effect = CalledProcessError("apt failure", "add-apt-repository")
         self.harness.begin()
-        self.harness.update_config({'ubuntu-advantage-token': 'test-token'})
-        self.assertEqual(_check_output.call_count, 1)
-        _check_output.assert_called_with(['ubuntu-advantage', 'status', '--format', 'json'])
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        with self.assertRaises(CalledProcessError):
+            self.harness.update_config({"ppa": "ppa:different-client/unstable"})
+        self.assertEqual(self.harness.charm._state.ppa, "ppa:ua-client/stable")
+        self.assertIsInstance(self.harness.model.unit.status, MaintenanceStatus)
+
+    @patch("subprocess.call")
+    @patch("subprocess.check_output")
+    @patch("subprocess.check_call")
+    def test_config_changed_reattach(self, _check_call, _check_output, _call):
+        success = {
+            "attached": True,
+            "services": [
+                {
+                    "name": "esm-apps",
+                    "status": "enabled"
+                },
+                {
+                    "name": "esm-infra",
+                    "status": "enabled"
+                },
+                {
+                    "name": "livepatch",
+                    "status": "enabled"
+                }
+            ]
+        }
+        _check_output.side_effect = [
+            '{"attached":true}',
+            '{"attached":false}',
+            json.dumps(success)
+        ]
+        _call.return_value = 0
+        self.harness.begin()
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        self.harness.charm._state.hashed_token = \
+            "4c5dc9b7708905f77f5e5d16316b5dfb425e68cb326dcd55a860e90a7707031e"
+        self.harness.update_config({"token": "test-token-2"})
         self.assertEqual(_check_call.call_count, 1)
-        _check_call.assert_called_with(['ubuntu-advantage', 'attach', 'test-token'])
+        _check_call.assert_has_calls([
+            call(["ubuntu-advantage", "detach", "--assume-yes"])
+        ])
+        self.assertEqual(_call.call_count, 1)
+        _call.assert_has_calls([
+            call(["ubuntu-advantage", "attach", "test-token-2"])
+        ])
+        self.assertEqual(self.harness.charm._state.ppa, "ppa:ua-client/stable")
+        self.assertEqual(self.harness.charm._state.hashed_token,
+                         "ab8a83efb364bf3f6739348519b53c8e8e0f7b4c06b6eeb881ad73dcf0059107")
         self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
+        self.assertEqual(self.harness.model.unit.status.message,
+                         "attached (esm-apps,esm-infra,livepatch)")
+
+    @patch("subprocess.call")
+    @patch("subprocess.check_output")
+    @patch("subprocess.check_call")
+    def test_config_changed_attach_failure(self, _check_call, _check_output, _call):
+        _check_output.side_effect = [
+            '{"attached":false}'
+        ]
+        _call.return_value = 1
+        self.harness.begin()
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        self.harness.update_config({"token": "test-token"})
+        self.assertIsInstance(self.harness.model.unit.status, BlockedStatus)
+        self.assertEqual(self.harness.model.unit.status.message,
+                         "Error attaching, possibly an invalid token?")
+
+    @patch("subprocess.call")
+    @patch("subprocess.check_output")
+    @patch("subprocess.check_call")
+    def test_config_changed_xenial_returned_bytes(self, _check_call, _check_output, _call):
+        success = {
+            "attached": True,
+            "services": [
+                {
+                    "name": "esm-apps",
+                    "status": "enabled"
+                },
+                {
+                    "name": "esm-infra",
+                    "status": "enabled"
+                },
+                {
+                    "name": "livepatch",
+                    "status": "enabled"
+                }
+            ]
+        }
+        _check_output.side_effect = [
+            bytes(json.dumps(success), "utf-8")
+        ]
+        self.harness.begin()
+        self.harness.charm._state.ppa = "ppa:ua-client/stable"
+        self.harness.charm._state.hashed_token = \
+            "4c5dc9b7708905f77f5e5d16316b5dfb425e68cb326dcd55a860e90a7707031e"
+        self.harness.update_config({"token": "test-token"})
+        self.assertIsInstance(self.harness.model.unit.status, ActiveStatus)
+        self.assertEqual(self.harness.model.unit.status.message,
+                         "attached (esm-apps,esm-infra,livepatch)")
