ac4b2788a975d4f5fb72c788dfdb907834bc348d
Colin Misare <colin.misare@canonical.com>
2021-02-10 16:05:21 -0700

    better way to handle default values

--- src/charm.py
+++ src/charm.py
@@ -70,9 +70,7 @@ class UbuntuAdvantageCharm(CharmBase):
 
     def _handle_ppa_state(self):
         """Handle installing/removing ppa based on configuration and state"""
-        ppa = self.config.get("ppa")
-        if ppa:
-            ppa = ppa.strip()
+        ppa = self.config.get("ppa", "").strip()
         old_ppa = self._state.ppa
         if old_ppa and old_ppa != ppa:
             logger.info("Removing previously installed ppa (%s)", old_ppa)
@@ -98,9 +96,7 @@ class UbuntuAdvantageCharm(CharmBase):
 
     def _handle_token_state(self):
         """Handle subscription attachment and status output based on configuration and state"""
-        token = self.config.get("token")
-        if token:
-            token = token.strip()
+        token = self.config.get("token", "").strip()
         old_hashed_token = self._state.hashed_token
         if not token:
             if old_hashed_token:
