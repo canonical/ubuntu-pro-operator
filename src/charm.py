c72bbc3ebe5a6fffda34639712eac6fdaf45a5fe
Grant Orndorff <grant.orndorff@canonical.com>
2024-08-29 15:04:37 -0400

    feat: config option to trust a special cert
    In some cases a special cert may need to be trusted for the pro client
    operations. Setting override-ssl-cert-file will set the environment
    variable SSL_CERT_FILE to the set value for all pro client subprocesses.

--- src/charm.py
+++ src/charm.py
@@ -47,10 +47,10 @@ def update_configuration(contract_url):
         f.truncate()
 
 
-def detach_subscription():
+def detach_subscription(env):
     """Detach from any ubuntu-advantage subscription."""
     logger.info("Detaching ubuntu-advantage subscription")
-    subprocess.check_call(["ubuntu-advantage", "detach", "--assume-yes"])
+    subprocess.check_call(["ubuntu-advantage", "detach", "--assume-yes"], env=env)
 
 
 def set_livepatch_server(server):
@@ -123,9 +123,9 @@ def disable_canonical_livepatch():
         raise ProcessExecutionError(result.args, result.returncode, stdout, stderr)
 
 
-def ua_enable_service(service):
+def ua_enable_service(service, env):
     """Enable a ubuntu-advantage service."""
-    subprocess.check_call(["ubuntu-advantage", "enable", service])
+    subprocess.check_call(["ubuntu-advantage", "enable", service], env=env)
 
 
 def parse_services(services_str):
@@ -153,7 +153,7 @@ def create_attach_config(token, services=None):
 
 
 @retry(ProcessExecutionError)
-def attach_subscription(token, services=None):
+def attach_subscription(token, env, services=None):
     """Attach an ubuntu-advantage subscription using the specified token and services."""
     if services:
         with create_attach_config(token, services) as attach_config_path:
@@ -161,10 +161,14 @@ def attach_subscription(token, services=None):
                 ["ubuntu-advantage", "attach", "--attach-config", attach_config_path],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
+                env=env,
             )
     else:
         result = subprocess.run(
-            ["ubuntu-advantage", "attach", token], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            ["ubuntu-advantage", "attach", token],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=env,
         )
     if result.returncode != 0:
         stdout = (
@@ -182,12 +186,13 @@ def attach_subscription(token, services=None):
 
 
 @retry(ProcessExecutionError)
-def get_status_output():
+def get_status_output(env):
     """Return the parsed output from ubuntu-advantage status."""
     result = subprocess.run(
         ["ubuntu-advantage", "status", "--all", "--format", "json"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        env=env,
     )
     if result.returncode != 0:
         stdout = (
@@ -226,6 +231,7 @@ class UbuntuAdvantageCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self._setup_proxy_env()
+        self._setup_ssl_env()
         self._state.set_default(
             contract_url=None,
             hashed_token=None,
@@ -239,14 +245,14 @@ class UbuntuAdvantageCharm(CharmBase):
 
     def _setup_proxy_env(self):
         """Setup proxy variables from model."""
-        self.env = dict(os.environ)
-        self.env["http_proxy"] = self.config.get("override-http-proxy") or self.env.get(
-            "JUJU_CHARM_HTTP_PROXY", ""
-        )
-        self.env["https_proxy"] = self.config.get("override-https-proxy") or self.env.get(
-            "JUJU_CHARM_HTTPS_PROXY", ""
-        )
-        self.env["no_proxy"] = self.env.get("JUJU_CHARM_NO_PROXY", "")
+        self.proxy_env = dict(os.environ)
+        self.proxy_env["http_proxy"] = self.config.get(
+            "override-http-proxy"
+        ) or self.proxy_env.get("JUJU_CHARM_HTTP_PROXY", "")
+        self.proxy_env["https_proxy"] = self.config.get(
+            "override-https-proxy"
+        ) or self.proxy_env.get("JUJU_CHARM_HTTPS_PROXY", "")
+        self.proxy_env["no_proxy"] = self.proxy_env.get("JUJU_CHARM_NO_PROXY", "")
 
         # The values for 'http_proxy' and 'https_proxy' will be used for the
         # PPA install/remove operations (passed as environment variables), as
@@ -255,15 +261,23 @@ class UbuntuAdvantageCharm(CharmBase):
         # operations (passed as an environment variable).
 
         # log proxy environment variables for debugging
-        for envvar, value in self.env.items():
+        for envvar, value in self.proxy_env.items():
             if envvar.endswith("proxy".lower()):
                 logger.debug("Envvar '%s' => '%s'", envvar, value)
+
+    def _setup_ssl_env(self):
+        """Setup openssl variables from model."""
+        self.ssl_env = dict(os.environ)
+        value = self.config.get("override-ssl-cert-file", "")
+        self.ssl_env["SSL_CERT_FILE"] = value
+        logger.debug("Envvar 'SSL_CERT_FILE' => '%s'", value)
 
     def config_changed(self, event):
         """Install and configure ubuntu-advantage tools and attachment."""
         logger.info("Beginning config_changed")
         self.unit.status = MaintenanceStatus("Configuring")
         self._setup_proxy_env()
+        self._setup_ssl_env()
         self._handle_ppa_state()
         self._handle_package_state()
         self._configure_livepatch()
@@ -282,14 +296,14 @@ class UbuntuAdvantageCharm(CharmBase):
 
         if old_ppa and old_ppa != ppa:
             logger.info("Removing previously installed ppa (%s)", old_ppa)
-            remove_ppa(old_ppa, self.env)
+            remove_ppa(old_ppa, self.proxy_env)
             self._state.ppa = None
             # If ppa is changed, want to remove the previous version of the package for consistency
             self._state.package_needs_installing = True
 
         if ppa and ppa != old_ppa:
             logger.info("Installing ppa: %s", ppa)
-            install_ppa(ppa, self.env)
+            install_ppa(ppa, self.proxy_env)
             self._state.ppa = ppa
             # If ppa is changed, want to force an install of the package for potential updates
             self._state.package_needs_installing = True
@@ -321,7 +335,7 @@ class UbuntuAdvantageCharm(CharmBase):
                 self._state.livepatch_installed and self._state.hashed_livepatch_token is not None
             ):
                 # Set to default server
-                status = get_status_output()
+                status = get_status_output(self.ssl_env)
                 is_attached = status.get("attached")
                 enabled_services = get_enabled_services(status)
                 logger.info("Setting livepatch on-prem server to default")
@@ -330,7 +344,7 @@ class UbuntuAdvantageCharm(CharmBase):
                 disable_canonical_livepatch()
                 if is_attached and "livepatch" in enabled_services:
                     logger.info("Enabling livepatch hosted server using Pro client")
-                    ua_enable_service("livepatch")
+                    ua_enable_service("livepatch", self.ssl_env)
                 self._state.hashed_livepatch_token = None
         except ProcessExecutionError as e:
             logger.error("Failed to configure livepatch: %s", str(e))
@@ -358,12 +372,12 @@ class UbuntuAdvantageCharm(CharmBase):
             self._state.contract_url = contract_url
 
         try:
-            status = get_status_output()
+            status = get_status_output(self.ssl_env)
         except ProcessExecutionError as e:
             self.unit.status = BlockedStatus(str(e))
             return
         if status["attached"] and (config_changed or token_changed):
-            detach_subscription()
+            detach_subscription(self.ssl_env)
             self._state.hashed_token = None
 
         if not token:
@@ -373,7 +387,7 @@ class UbuntuAdvantageCharm(CharmBase):
             logger.info("Attaching ubuntu-advantage subscription")
             try:
                 enable_services = parse_services(self.config.get("services", "").strip())
-                attach_subscription(token, services=enable_services)
+                attach_subscription(token, self.ssl_env, services=enable_services)
             except ProcessExecutionError as e:
                 self.unit.status = BlockedStatus(str(e))
                 return
@@ -381,7 +395,7 @@ class UbuntuAdvantageCharm(CharmBase):
 
     def _handle_status_state(self):
         """Parse status output to determine which services are active."""
-        status = get_status_output()
+        status = get_status_output(self.ssl_env)
         services = get_enabled_services(status)
         message = "Attached (" + ",".join(services) + ")"
         self.unit.status = ActiveStatus(message)
@@ -389,13 +403,13 @@ class UbuntuAdvantageCharm(CharmBase):
     def _configure_ua_proxy(self):
         """Configure the proxy options for the ubuntu-advantage client."""
         for config_key in ("http_proxy", "https_proxy"):
-            if self.env[config_key]:
+            if self.proxy_env[config_key]:
                 subprocess.check_call(
                     [
                         "ubuntu-advantage",
                         "config",
                         "set",
-                        "{}={}".format(config_key, self.env[config_key]),
+                        "{}={}".format(config_key, self.proxy_env[config_key]),
                     ]
                 )
             else:
