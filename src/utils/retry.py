3777e93ca40ea3ef09e49368656c6173933cee5d
Grant Orndorff <grant.orndorff@canonical.com>
2025-06-18 11:42:10 -0400

    detach before retrying an attach
    If an attach was partially successful, then it was a failure. Prior to
    this change the retries would all fail because the machine was "already
    attached", even though the original attach was partial (i.e. not all
    services were successfully enabled). Now, we detach, disabling any
    services that got enabled in a partial attach, before trying to attach
    again.
    
    LP: #2111310

--- src/utils/retry.py
+++ src/utils/retry.py
@@ -12,8 +12,13 @@ logger = logging.getLogger(__name__)
 RETRY_SLEEPS = [0.5, 1, 2]
 
 
-def retry(exception):
-    """Decorator to retry on exception multiple times with a sleep in between."""
+def retry(exception, compensating_action=None):
+    """Decorator to retry on exception multiple times with a sleep in between.
+
+    When compensating_action is defined, it is a function that is run with the
+    same arguments as the original function after the exception occurs, before
+    sleeping and retrying.
+    """
 
     def wrapper(func):
         @wraps(func)
@@ -26,6 +31,9 @@ def retry(exception):
                     if remaining_retries == 0:
                         raise e
                     logger.warning("%s: Retrying %d more times.", str(e), remaining_retries)
+                    if compensating_action:
+                        logger.info("Running compensating action.")
+                        compensating_action(*args, **kwargs)
                     time.sleep(sleep_time)
 
         return decorator
